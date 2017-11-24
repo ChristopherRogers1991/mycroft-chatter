822503f134081d0750936f1695cc99cbeec158cb
Chris Rogers <ChristopherRogers1991@gmail.com>
2017-11-24 15:01:50 -0500

    remove more nickname cruft

--- client/ui/generated/chat.py
+++ client/ui/generated/chat.py
@@ -14,73 +14,101 @@ https://www.riverbankcomputing.com/software/pyqt/download5/
 #
 # Created by: PyQt5 UI code generator 5.9.1
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 
 
 class Ui_ChatMainWindow(QtWidgets.QMainWindow):
+
     def setupUi(self, ChatMainWindow):
+
         ChatMainWindow.setObjectName("ChatMainWindow")
         ChatMainWindow.resize(800, 600)
+
         self.centralwidget = QtWidgets.QWidget(ChatMainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.hboxlayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.hboxlayout.setContentsMargins(9, 9, 9, 9)
         self.hboxlayout.setSpacing(6)
         self.hboxlayout.setObjectName("hboxlayout")
+
         self.vboxlayout = QtWidgets.QVBoxLayout()
         self.vboxlayout.setContentsMargins(0, 0, 0, 0)
         self.vboxlayout.setSpacing(6)
         self.vboxlayout.setObjectName("vboxlayout")
+
         self.chatHistory = QtWidgets.QTextBrowser(self.centralwidget)
         self.chatHistory.setAcceptDrops(False)
         self.chatHistory.setAcceptRichText(True)
         self.chatHistory.setObjectName("chatHistory")
+
         self.vboxlayout.addWidget(self.chatHistory)
+
         self.hboxlayout1 = QtWidgets.QHBoxLayout()
         self.hboxlayout1.setContentsMargins(0, 0, 0, 0)
         self.hboxlayout1.setSpacing(6)
         self.hboxlayout1.setObjectName("hboxlayout1")
+
         self.messageLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.messageLineEdit.setObjectName("messageLineEdit")
+
         self.hboxlayout1.addWidget(self.messageLineEdit)
+
         self.sendButton = QtWidgets.QPushButton(self.centralwidget)
+
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.sendButton.sizePolicy().hasHeightForWidth())
+
         self.sendButton.setSizePolicy(sizePolicy)
         self.sendButton.setWhatsThis("")
         self.sendButton.setObjectName("sendButton")
         self.hboxlayout1.addWidget(self.sendButton)
+
         self.vboxlayout.addLayout(self.hboxlayout1)
+
         self.hboxlayout.addLayout(self.vboxlayout)
+
         ChatMainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(ChatMainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
         self.menubar.setObjectName("menubar")
+
         self.menuQuit = QtWidgets.QMenu(self.menubar)
         self.menuQuit.setObjectName("menuQuit")
+
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
+
         ChatMainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(ChatMainWindow)
         self.statusbar.setObjectName("statusbar")
+
         ChatMainWindow.setStatusBar(self.statusbar)
+
         self.actionQuit = QtWidgets.QAction(ChatMainWindow)
         self.actionQuit.setObjectName("actionQuit")
+
         self.actionAboutQt = QtWidgets.QAction(ChatMainWindow)
         self.actionAboutQt.setObjectName("actionAboutQt")
-        self.actionChangeNickname = QtWidgets.QAction(ChatMainWindow)
-        self.actionChangeNickname.setObjectName("actionChangeNickname")
+
         self.menuQuit.addAction(self.actionAboutQt)
+
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionQuit)
+
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuQuit.menuAction())
 
         self.retranslateUi(ChatMainWindow)
+
         self.messageLineEdit.returnPressed.connect(self.sendButton.animateClick)
+
         self.actionQuit.triggered['bool'].connect(ChatMainWindow.close)
+
         QtCore.QMetaObject.connectSlotsByName(ChatMainWindow)
         ChatMainWindow.setTabOrder(self.chatHistory, self.messageLineEdit)
         ChatMainWindow.setTabOrder(self.messageLineEdit, self.sendButton)
